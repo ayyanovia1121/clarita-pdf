3b491a6c9e4ea55a8b708a93eacdfd06974de316
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-12 13:13:47 +0700

    refactor(ui): ♻️ include Header component on Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Source_Sans_3 as FontSans } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const fontSans = FontSans({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body
         className={`${fontSans.variable} font-sans antialiased`}
       >
+        <Header />
         {children}
       </body>
     </html>
