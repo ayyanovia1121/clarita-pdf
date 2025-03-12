6a51a6814a168e5351598f685a05758b63ebbb16
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-12 14:05:43 +0700

    refactor(ui): ♻️ add className property

--- src/components/common/nav-link.tsx
+++ src/components/common/nav-link.tsx
@@ -2,10 +2,12 @@ import Link from "next/link"
 
 const NavLink = ({
     href,
-    children
+    children,
+    className,
 }: {
     href: string,
-    children: React.ReactNode
+    children: React.ReactNode,
+    className?: string
 }) => {
   return <Link href={href} className="transition-colors text-sm duration-200 text-gray-600 hover:text-rose-500">{children}</Link>
 }
