8d2790cef76a89855399c56964c3225664415ec6
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-12 14:16:10 +0700

    feat(ui): 🎨 add pathname and active state support to NavLink

--- src/components/common/nav-link.tsx
+++ src/components/common/nav-link.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavLink = ({
   href,
@@ -10,12 +13,15 @@ const NavLink = ({
   children: React.ReactNode;
   className?: string;
 }) => {
+  const pathname = usePathname()
+  const isActive = pathname === href || (href !== '/' && pathname.startsWith(href));
   return (
     <Link
       href={href}
       className={cn(
         "transition-colors text-sm duration-200 text-gray-600 hover:text-rose-500",
-        className
+        className,
+        isActive && "text-rose-500"
       )}
     >
       {children}
