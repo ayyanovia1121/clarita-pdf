9bac2199758b43e250d41f6911e9438004283ae5
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-15 12:58:52 +0700

    refactor(ui): ♻️ add Hero section component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import HeroSection from "@/components/home/hero-section";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="relative w-full">
+      <HeroSection />
     </div>
   );
 }
